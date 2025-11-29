8393142c36e25597af37bdfef67fa9c62779cbf2
Abolfzl80 <abol95867@gmail.com>
2025-11-29 19:01:41 +0330

    debug some file:)

--- script/backup.sh
+++ script/backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-des=$(cut -f3 ./backup.conf | head -1 | tail -1)
-s=$(cut -f3 ./backup.conf | head -2 | tail -1)
-#echo "des: ${des}, s: ${s}"
-echo $(mkdir ${s}/$(date +%Y-%m-%d-%H-%M-%S))
+s=$(cut -f3 ./backup.conf | head -1 | tail -1)
+des=$(cut -f3 ./backup.conf | head -2 | tail -1)
+f=$(mkdir ${des}/$(date +%Y-%m-%d-%H-%M-%S))
+b=$(cp -r ${s}/ ${des}/$(date +%Y-%m-%d-%H-%M-%S))
+echo ${b}
 echo
 echo "success!"
